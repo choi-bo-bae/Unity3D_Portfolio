2b0ee5ddc0085cf13485bccb9a1cc23f6cb03cd2
qhqo727@gmai.com <qhqo727@gmail.com>
2020-06-18 17:41:10 +0900

    지형 거의 완성
    -땅 다지기 완성
    
    -컨셉은 탈옥으로
    -죄수가 플레이어, 경찰을 암살한다.
    -초기 기획은 분명히 어쌔신크리드였는데...
    -무슨 게임이 되어가고있는거지..
    
    할 일
    
    -등대 이미지 찾기
    -경찰, 죄수 이미지 찾기
    -더 짧은 단검 찾기
    -오토로 돌릴 동료 1명 만들기

---
+++ Unity3DPortfolio/Assets/_CBB/Scripts/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float h;
+    private float v;
+
+
+    public float speed = 25.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        transform.Translate(moveDir.normalized * speed * Time.deltaTime);
+
+       // dir = Camera.main.transform.TransformDirection(dir);
+    }
+
+
+}
